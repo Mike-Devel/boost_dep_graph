146c2febab21666941b7c3e3d11fe08f82394e57
Mike Dev <mike.dev@gmx.de>
2019-03-04 13:54:16 +0100

    [gui] Give more vertical space

--- src/ui/gui_cfg.hpp
+++ src/ui/gui_cfg.hpp
@@ -8,10 +8,10 @@ namespace mdev::bdg::gui::cfg {
 constexpr double arrow_size  = 10;
 constexpr double node_radius = 10;
 
-constexpr double min_node_dist = 15; // ~The minimum distance between two nodes we try to hold
+constexpr double min_node_dist = 12; // ~The minimum distance between two nodes we try to hold
 
 const QFont         module_name_font = QFont( "Helvetica", 15 );
 constexpr QMarginsF margins( 20, 50, 80, 20 );
-constexpr QSizeF    scene_size = QSizeF{1800, 1000};
+constexpr QSizeF    scene_size = QSizeF{1800, 1200};
 
 } // namespace mdev::bdg::gui::cfg
